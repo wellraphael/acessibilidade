6e001e12b464e5141b85efa481ec7b66e950f950
wellraphael <166656921+wellraphael@users.noreply.github.com>
2025-03-24 11:38:40 -0300

    .

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-document.addEventListener('DOMcontentLoaded', function(){
-const aumentaFonteBotao = document.getElementByid('aumentar-fonte');
-const diminuiFonteBotao = document.getElementByid('diminuir-fonte');
-let tamanhoAtualFonte = 1;
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-aumentaFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte + = 0.1;
-    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
+    });
 
-})
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
-diminuiFonteBotao.addEventListener('click', function(){
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+    });
 
-})
-
-})
+});
